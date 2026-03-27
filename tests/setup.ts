d6f53c608b276a4b124fd4770fcc83222466973c
Kassaila <kasela.kassaila@gmail.com>
2026-03-27 19:05:15 +0200

    chore: remove old tests/ directory

--- tests/setup.ts
+++
@@ -1,6 +0,0 @@
-import { RuleTester } from '@typescript-eslint/rule-tester';
-import { afterAll, describe, it } from 'vitest';
-
-RuleTester.afterAll = afterAll;
-RuleTester.describe = describe;
-RuleTester.it = it;
